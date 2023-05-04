b5af23445c3c2b9c0dce241c36cb56c8a320b64d
AliGaygisiz <aligaygisiz34@gmail.com>
2023-05-04 21:00:55 +0300

    neorg fixed

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,35 +1,34 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin"
-  },
-  {
-      "nvim-neorg/neorg",
-      event = "BufRead",
-      build = ":Neorg sync-parsers",
-      opts = {
-          load = {
-              ["core.defaults"] = {}, -- Loads default behaviour
-              ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
-              ["core.norg.dirman"] = { -- Manages Neorg workspaces
-                  config = {
-                      workspaces = {
-                          notes = "~/notes",
-                      },
-                  },
-              },
-          },
-      },
-      dependencies = { { "nvim-lua/plenary.nvim" } },
-  }
+    -- You can also add new plugins here as well:
+    -- Add plugins, the lazy syntax
+    -- "andweeb/presence.nvim",
+    -- {
+    --   "ray-x/lsp_signature.nvim",
+    --   event = "BufRead",
+    --   config = function()
+    --     require("lsp_signature").setup()
+    --   end,
+    -- },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin"
+    },
+    {
+        "nvim-neorg/neorg",
+        build = ":Neorg sync-parsers",
+        opts = {
+            load = {
+                ["core.defaults"] = {},  -- Loads default behaviour
+                ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                ["core.dirman"] = {      -- Manages Neorg workspaces
+                    config = {
+                        workspaces = {
+                            notes = "~/notes",
+                        },
+                    },
+                },
+            },
+        },
+        dependencies = { { "nvim-lua/plenary.nvim" } },
+    }
 }
